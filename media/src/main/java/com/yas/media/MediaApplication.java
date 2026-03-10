0edc76e0b4e6060247e92c12867cf0231fb9fa82
NguyenThanhTien539 <161334882+NguyenThanhTien539@users.noreply.github.com>
2026-03-10 08:26:16 +0700

    add error for gitleak

--- media/src/main/java/com/yas/media/MediaApplication.java
+++ media/src/main/java/com/yas/media/MediaApplication.java
@@ -13,5 +13,5 @@ public class MediaApplication {
     public static void main(String[] args) {
         SpringApplication.run(MediaApplication.class, args);
     }
-    // TODO: Add logging configuration and other necessary configurations for the application.
+    private static final String API_KEY = "1234567890abcdef1234567890abcdef";
 }
